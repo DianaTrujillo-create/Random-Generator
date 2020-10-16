342989ff8f7f4dadfa70401747378fb8be4ac4b5
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2020-10-15 21:04:28 -0500

    commit to main

---
+++ script.js
@@ -0,0 +1,119 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// Write password to the #password input
+
+
+// Possible password values
+
+var charUpper = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
+var charLower = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
+var Numbers = "0123456789";
+var specialchar = "!@#$%^&*()~"
+
+
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+
+//generate random password 
+function generatePassword() {
+
+
+  // Option variables
+  var passwordLength = "";
+  var Upper;
+  var Lower;
+  var Special;
+  var Num;
+
+
+
+  //Asks the user for their input 
+
+  var passwordLength = prompt("How many characters would you like your password to contain? Choose between 8 and 128");
+
+ // loop if user inputs outside of the criteria
+
+  if (passwordLength < 8) {
+    alert("The password must be at least 8 characters, try again!");
+    return;
+}
+  if (passwordLength > 128) {
+    alert("The password must be less than 128 characters, try again!");
+    return;
+}
+
+  var Upper = confirm("Click OK to confirm if you would like to include any uppercase letters");
+  var Lower = confirm ("Click OK to confirm if you would like to include lowercase letters");
+  var Special = confirm("Click OK to confirm if you would like to include speical characters"); 
+  var Num = confirm("Click OK to confirm if you would like to include any numbers");
+
+
+  // Loop if user inputs outside of the criteria 
+
+
+  if (Upper === false && Lower === false && Special === false && Num === false) {
+    alert("Your password must contain at least one uppercase, lowercase, special, or numeric character, try again!");
+    var Upper = confirm("Click OK to confirm if you would like to include any uppercase letters");
+    var Lower = confirm ("Click OK to confirm if you would like to include lowercase letters");
+    var Special = confirm("Click OK to confirm if you would like to include speical characters"); 
+    var Num = confirm("Click OK to confirm if you would like to include any numbers");
+
+}
+
+
+
+
+}
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
